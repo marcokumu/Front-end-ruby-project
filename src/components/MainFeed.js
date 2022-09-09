1cfa962564dcfd320e7a23fb813e98a81ee19794
Okumu <marcokumu10@gmail.com>
2022-09-09 21:25:04 +0000

    Still loading message

--- src/components/MainFeed.js
+++ src/components/MainFeed.js
@@ -24,7 +24,7 @@ function MainFeed({ pokemons }) {
           ))
         ) : (
           <>
-            <p>Nothing here</p>
+            <p>Still loading, please wait...</p>
           </>
         )}
       </ul>
