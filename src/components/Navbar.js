0d6a04f090d034fb4db173e4338449322fd804bb
Okumu <marcokumu10@gmail.com>
2022-09-14 07:15:13 +0000

    new review component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,35 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
-// function Navbar() {
-//   return (
-//     <>
-//       <nav>
-//         <div className="div-header">
-//           <div
-//             style={{
-//               display: "flex",
-//               flexDirection: "row",
-//               alignItems: "center",
-//             }}
-//           >
-//             <Link to="/" className="h2">Pokemon</Link>
-//           </div>
-//           <div
-//             style={{
-//               display: "flex",
-//               flexDirection: "row",
-//               alignItems: "center",
-//             }}
-//           >
-//             <Link to="/pokemon" className="btn">
-//               Poke
-//             </Link>
-//           </div>
-//         </div>
-//       </nav>
-//     </>
-//   );
-// }
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <div className="div-header">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Link to="/" className="h2">Pokemon</Link>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Link to="/pokemon" className="btn">
+              Poke
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
 
-// export default Navbar;
+export default Navbar;
