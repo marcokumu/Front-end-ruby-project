ffd20b94d2075c652172d850fdbc8990e2c6cb4b
Okumu <marcokumu10@gmail.com>
2022-09-14 07:12:50 +0000

    delete review component

--- src/components/DeleteReview.js
+++ src/components/DeleteReview.js
@@ -1,34 +1,34 @@
 
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 
-function DeleteReview({ reviews_id }) {
-  const { id } = useParams();
+// function DeleteReview({ reviews_id }) {
+//   const { id } = useParams();
 
-  function handleDelete(e) {
-    e.preventDefault();
-    fetch(`https://ruby-pokemon.herokuapp.com/reviews/${reviews_id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((r) => {
-      if (r.ok) {
-        window.location.reload();
-      } else {
-        r.json();
-      }
-    });
-  }
+//   function handleDelete(e) {
+//     e.preventDefault();
+//     fetch(`https://ruby-pokemon.herokuapp.com/reviews/${reviews_id}`, {
+//       method: "DELETE",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     }).then((r) => {
+//       if (r.ok) {
+//         window.location.reload();
+//       } else {
+//         r.json();
+//       }
+//     });
+//   }
 
-  console.log(reviews_id);
+//   console.log(reviews_id);
 
-  return (
-    <div>
-      <button className="btn" onClick={handleDelete}>
-        Delete
-      </button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <button className="btn" onClick={handleDelete}>
+//         Delete
+//       </button>
+//     </div>
+//   );
+// }
 
-export default DeleteReview;
+// export default DeleteReview;
