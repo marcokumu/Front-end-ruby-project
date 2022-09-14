29d3c2594df08a6787e1af6d758ade7cf964ce9e
Okumu <marcokumu10@gmail.com>
2022-09-14 07:11:04 +0000

    home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
-import React from "react";
+// import React from "react";
 
-function Home() {
-  return (
-    <div className="comment">
-      <img
-        src="https://cdn.pixabay.com/photo/2016/07/13/08/31/pokemon-1513925_960_720.jpg"
-        alt=""
-      ></img>
-    </div>
-  );
-}
+// function Home() {
+//   return (
+//     <div className="comment">
+//       <img
+//         src="https://cdn.pixabay.com/photo/2016/07/13/08/31/pokemon-1513925_960_720.jpg"
+//         alt=""
+//       ></img>
+//     </div>
+//   );
+// }
 
-export default Home;
+// export default Home;
