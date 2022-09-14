d0a3da938b37a6a81ef5e007f290978d02d67167
Okumu <marcokumu10@gmail.com>
2022-09-14 07:18:16 +0000

    app component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import { useEffect, useState } from "react";
-import Navbar from "./components/Navbar";
-import MainFeed from "./components/MainFeed";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Pokemon from "./components/Pokemon";
-import Home from "./components/Home";
+// import { useEffect, useState } from "react";
+// import Navbar from "./components/Navbar";
+// import MainFeed from "./components/MainFeed";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Pokemon from "./components/Pokemon";
+// import Home from "./components/Home";
 
-function App() {
-  const [pokemons, setPokemon] = useState([]);
+// function App() {
+//   const [pokemons, setPokemon] = useState([]);
 
-  useEffect(() => {
-    fetch("https://ruby-pokemon.herokuapp.com/pokemon")
-      .then((r) => r.json())
-      .then((pokemon) => setPokemon(pokemon));
-  }, []);
+//   useEffect(() => {
+//     fetch("https://ruby-pokemon.herokuapp.com/pokemon")
+//       .then((r) => r.json())
+//       .then((pokemon) => setPokemon(pokemon));
+//   }, []);
 
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route
-            path="/pokemon"
-            element={<MainFeed pokemons={pokemons} />}
-          ></Route>
-          <Route path="/pokemon/:id" element={<Pokemon />}></Route>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
-}
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Navbar />
+//         <Routes>
+//           <Route
+//             path="/pokemon"
+//             element={<MainFeed pokemons={pokemons} />}
+//           ></Route>
+//           <Route path="/pokemon/:id" element={<Pokemon />}></Route>
+//           <Route path="/" element={<Home />}></Route>
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/components/ReviewFeed.js
+++ src/components/ReviewFeed.js
@@ -1,47 +1,47 @@
-// import React from "react";
-// import { useEffect, useState } from "react";
-// import { useParams, useNavigate } from "react-router-dom";
-// import DeleteReview from "./DeleteReview";
+import React from "react";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import DeleteReview from "./DeleteReview";
 
-// function ReviewFeed({ reviews }) {
-//   const navigate = useNavigate();
-//   const { id } = useParams();
-//   console.log(reviews);
+function ReviewFeed({ reviews }) {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  console.log(reviews);
 
-//   function handleDelete(e) {
-//     // e.preventDefault()
-//     fetch(`https://ruby-pokemon.herokuapp.com/reviews/${reviews.id}`, {
-//       method: "DELETE",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     }).then((r) => {
-//       if (r.ok) {
-//         navigate(`/pokemon/${id}/reviews`);
-//       } else {
-//         r.json();
-//       }
-//     });
-//   }
-//   handleDelete();
+  function handleDelete(e) {
+    // e.preventDefault()
+    fetch(`https://ruby-pokemon.herokuapp.com/reviews/${reviews.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((r) => {
+      if (r.ok) {
+        navigate(`/pokemon/${id}/reviews`);
+      } else {
+        r.json();
+      }
+    });
+  }
+  handleDelete();
 
-//   return (
-//     <div className="comment">
-//       <ol className="comment-body">
-//         {reviews.map((review) => (
-//           <li className="card">
-//             <h3>Anonymous:</h3>
-//             <p className="div-feed">{review.text}</p>
-//             <p>
-//               <strong>Rating: </strong>
-//               {review.rating}
-//             </p>
-//             <DeleteReview reviews_id={review.id} />
-//           </li>
-//         ))}
-//       </ol>
-//     </div>
-//   );
-// }
+  return (
+    <div className="comment">
+      <ol className="comment-body">
+        {reviews.map((review) => (
+          <li className="card">
+            <h3>Anonymous:</h3>
+            <p className="div-feed">{review.text}</p>
+            <p>
+              <strong>Rating: </strong>
+              {review.rating}
+            </p>
+            <DeleteReview reviews_id={review.id} />
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
 
-// export default ReviewFeed;
+export default ReviewFeed;
