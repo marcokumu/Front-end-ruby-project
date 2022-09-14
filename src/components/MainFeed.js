4865bc5e99de2dc58eaf9c1b1ee6f1d0709e493a
Okumu <marcokumu10@gmail.com>
2022-09-14 07:14:26 +0000

    navbar component

--- src/components/MainFeed.js
+++ src/components/MainFeed.js
@@ -1,35 +1,35 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
-// function MainFeed({ pokemons }) {
-//   return (
-//     <div className="mainfeed">
-//       <ul style={{ display: "flex", flexWrap: "wrap" }}>
-//         {pokemons.length > 0 ? (
-//           pokemons.map((pokemon) => (
-//             <div className="card">
-//               <article>
-//                 <h1>{pokemon.name}</h1>
-//                 <p>
-//                   <strong>Location:</strong> <em>{pokemon.location}</em>
-//                 </p>
-//                 <p>
-//                   <strong>Signature Move:</strong> <em>{pokemon.move}</em>
-//                 </p>
-//               </article>
-//               <Link className="btn" to={`/pokemon/${pokemon.id}`}>
-//                 View
-//               </Link>
-//             </div>
-//           ))
-//         ) : (
-//           <>
-//             <p className="loading-text">Still loading, please wait...</p>
-//           </>
-//         )}
-//       </ul>
-//     </div>
-//   );
-// }
+function MainFeed({ pokemons }) {
+  return (
+    <div className="mainfeed">
+      <ul style={{ display: "flex", flexWrap: "wrap" }}>
+        {pokemons.length > 0 ? (
+          pokemons.map((pokemon) => (
+            <div className="card">
+              <article>
+                <h1>{pokemon.name}</h1>
+                <p>
+                  <strong>Location:</strong> <em>{pokemon.location}</em>
+                </p>
+                <p>
+                  <strong>Signature Move:</strong> <em>{pokemon.move}</em>
+                </p>
+              </article>
+              <Link className="btn" to={`/pokemon/${pokemon.id}`}>
+                View
+              </Link>
+            </div>
+          ))
+        ) : (
+          <>
+            <p className="loading-text">Still loading, please wait...</p>
+          </>
+        )}
+      </ul>
+    </div>
+  );
+}
 
-// export default MainFeed;
+export default MainFeed;
